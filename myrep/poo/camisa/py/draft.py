4e8516406f6d01548728a7c1393f82cac28ce6c1
RaiVitorianoNogueira <rainogueira2019@gmail.com>
2025-10-21 14:45:58 +0000

    camisa

---
+++ myrep/poo/camisa/py/draft.py
@@ -0,0 +1,21 @@
+class Camisa:
+    def __init__(self):
+        self.__tamanho: str = ""
+
+    def getTamanho(self) -> str:
+
+        return self.__tamanho
+
+    def setTamanho(self, valor: str):
+
+
+
+def main()
+
+    while 
+
+
+
+
+
+main()
